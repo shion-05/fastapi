f36bae730395e8c466008eeebe2e800a58739f02
shion-05 <coluna326515@gmail.com>
2025-10-21 11:52:54 +0900

    クエリパラメータの取得実装（任意，混合）

--- main.py
+++ main.py
@@ -12,3 +12,13 @@ def read_root():
 @app.get("/get_name")
 def get_name(name: str = Query(...)): #...で「必須」
     return {"name": name}
+
+#クエリパラメータの取得（任意）
+@app.get("/get_age")
+def get_age(age: int = Query(None)):#Noneで「任意」
+    return {"age": age}
+
+#クエリパラメータの取得（混合）
+@app.get("/get_profile")
+def get_profile(name: str = Query(...), age: int = Query(None)):
+    return {"name": name, "age": age}
