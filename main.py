0a896c641edb9386eb85ba73a12b039fa74c6e83
shion-05 <coluna326515@gmail.com>
2025-10-21 11:47:02 +0900

    クエリパラメータの取得実装（必須）

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+@app.get("/get_name")
+def get_name(name: str = Query(...)):
+    return {"name": name}
