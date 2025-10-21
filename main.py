2c357a571dd7505a29972853dcf5a51d519ecfdd
shion-05 <coluna326515@gmail.com>
2025-10-21 11:49:45 +0900

    コメント追加

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from fastapi import FastAPI, Query
 
 app = FastAPI()
 
+#Hello, World! を返すエンドポイント定義
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
 
+#クエリパラメータの取得（必須）
 @app.get("/get_name")
-def get_name(name: str = Query(...)):
+def get_name(name: str = Query(...)): #...で「必須」
     return {"name": name}
