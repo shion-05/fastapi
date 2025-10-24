b2da79b680b771b770f05d6b740ceefa3c288d67
shion-05 <coluna326515@gmail.com>
2025-10-24 10:47:45 +0900

    まとめの作成

--- main.py
+++ main.py
@@ -27,3 +27,12 @@ def get_profile(name: str = Query(...), age: int = Query(None)):
 @app.get('/get_name2/{name2}/myname')
 def get_name2(name2: str):
     return {"name": name2}
+
+#まとめ
+@app.get("/get_profile/{name2}/myname")
+def get_profile(
+    name2: str,                               # パスパラメータは非デフォルトを先にする（name2が非デフォルト）
+    name: str = Query(...),                   # 必須クエリ
+    age: int | None = Query(None)             # 任意クエリ
+):
+    return {"name": name, "name2": name2, "age": age}
